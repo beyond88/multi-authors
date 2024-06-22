8560599c047508d5fbdde0da9040af4df3fceb2f
beyond88 <muhin.cse.diu@gmail.com>
2024-06-22 12:58:46 +0600

    display author list

--- includes/Admin.php
+++ includes/Admin.php
@@ -1,8 +1,8 @@
-<?php
+<?php 
 namespace MultiAuthors;
 
 /**
- * The admin class
+ * The admin class for handling contributors metabox
  */
 class Admin {
 
@@ -10,11 +10,83 @@ class Admin {
      * Initialize the class
      * 
      * @since   1.0.0
-     * @access  public
-     * @param   none
-     * @return  void
      */
-    function __construct() {
-        $main = new Admin\Main();
+    public function __construct() {
+        add_action( 'add_meta_boxes', array( $this, 'add_contributors_metabox' ) );
+        add_action( 'save_post', array( $this, 'save_contributors_metabox' ) );
+    }
+
+    /**
+     * Add contributors metabox to post editor
+     * 
+     * @since   1.0.0
+     */
+    public function add_contributors_metabox() {
+        add_meta_box(
+            'ma_contributors',
+            'Contributors',
+            array( $this, 'contributors_metabox_callback' ),
+            'post',
+            'side',
+            'high'
+        );
+    }
+
+    /**
+     * Callback function to display contributors metabox content
+     * 
+     * @since   1.0.0
+     * 
+     * @param   WP_Post $post The post object
+     */
+    public function contributors_metabox_callback( $post ) {
+        // Add nonce field for security
+        wp_nonce_field( 'ma_save_contributors', 'ma_contributors_nonce' );
+
+        // Get the list of all authors
+        $users = get_users();
+
+        // Get current contributors
+        $current_contributors = get_post_meta( $post->ID, '_ma_contributors', true );
+
+        foreach ( $users as $user ) {
+            $checked = is_array( $current_contributors ) && in_array( $user->ID, $current_contributors ) ? 'checked' : '';
+            ?>
+            <label>
+                <input type="checkbox" name="ma_contributors[]" value="<?php echo esc_attr( $user->ID ); ?>" <?php echo $checked; ?>>
+                <?php echo esc_html( $user->display_name ); ?>
+            </label><br>
+            <?php
+        }
+    }
+
+    /**
+     * Save contributors data when post is saved
+     * 
+     * @since   1.0.0
+     * 
+     * @param   int $post_id The post ID
+     */
+    public function save_contributors_metabox( $post_id ) {
+        // Check nonce
+        if ( ! isset( $_POST['ma_contributors_nonce'] ) || ! wp_verify_nonce( $_POST['ma_contributors_nonce'], 'ma_save_contributors' ) ) {
+            return;
+        }
+
+        // Check autosave
+        if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+            return;
+        }
+
+        // Check permissions
+        if ( isset( $_POST['post_type'] ) && 'post' == $_POST['post_type'] ) {
+            if ( ! current_user_can( 'edit_post', $post_id ) ) {
+                return;
+            }
+        }
+
+        // Save contributors
+        $contributors = isset( $_POST['ma_contributors'] ) ? array_map( 'sanitize_text_field', $_POST['ma_contributors'] ) : array();
+        update_post_meta( $post_id, '_ma_contributors', $contributors );
     }
 }
--- includes/Frontend.php
+++ includes/Frontend.php
@@ -2,21 +2,56 @@
 namespace MultiAuthors;
 
 /**
-* Frontend handler class
-* 
-* @since    1.0.0
-* @param    none
-* @return   object
-*/
+ * Frontend handler class for displaying contributors
+ * 
+ * @since    1.0.0
+ */
 class Frontend {
 
     /**
-    * Initialize the class
-    *
-    * @since    1.0.0
-    * @param    none
-    * @return   object
-    */
-    function __construct() {
+     * Initialize the class
+     *
+     * @since    1.0.0
+     */
+    public function __construct() {
+        add_filter('the_content', array( $this, 'ma_display_contributors' ) );
+    }
+
+    /**
+     * Display contributors box at the end of single post content
+     *
+     * @since    1.0.0
+     *
+     * @param    string $content The post content
+     * @return   string Modified content with contributors box
+     */
+    public function ma_display_contributors($content) {
+        if (is_single() && is_main_query()) {
+            global $post;
+            $contributors = get_post_meta($post->ID, '_ma_contributors', true);
+    
+            if (!empty($contributors)) {
+                $content .= '<div class="ma-contributors-box">';
+                $content .= '<h3>'.__('Contributors', 'multi-authors').'</h3>';
+                $content .= '<ul>';
+    
+                foreach ($contributors as $contributor_id) {
+                    $user_info = get_userdata($contributor_id);
+                    $avatar = get_avatar($contributor_id, 32);
+                    $author_link = get_author_posts_url($contributor_id);
+                    $content .= '<li>';
+                    $content .= '<a href="' . esc_url($author_link) . '">';
+                    $content .= $avatar;
+                    $content .= esc_html($user_info->display_name);
+                    $content .= '</a>';
+                    $content .= '</li>';
+                }
+    
+                $content .= '</ul>';
+                $content .= '</div>';
+            }
+        }
+    
+        return $content;
     }
 }
